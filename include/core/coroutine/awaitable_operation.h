d0b884bf306f0d445b2940b8cdc78869f17b6baf
Hackman.Lo <admin@yytec.net>
2024-07-15 11:30:39 +0800

    Add 500 Internal server error

--- include/core/coroutine/awaitable_operation.h
+++ include/core/coroutine/awaitable_operation.h
@@ -6,16 +6,16 @@
 #define AWAITABLE_OPERATION_H
 
 namespace obelisk::core::coroutine {
-        template<typename ReturnType>
-        struct awaitable_operation : boost::cobalt::enable_awaitables<ReturnType> {
-            virtual bool await_ready() const noexcept { return false; }
-
-            virtual ~awaitable_operation() = default;
-
-            virtual void await_suspend(std::coroutine_handle<> coro) = 0;
-
-            virtual ReturnType await_resume() const noexcept =0;
-        }; // struct awaitable_operation
+        // template<typename ReturnType>
+        // struct awaitable_operation : boost::cobalt::enable_awaitables<ReturnType> {
+        //     virtual bool await_ready() const noexcept { return false; }
+        //
+        //     virtual ~awaitable_operation() = default;
+        //
+        //     virtual void await_suspend(std::coroutine_handle<> coro) = 0;
+        //
+        //     virtual ReturnType await_resume() const noexcept =0;
+        // }; // struct awaitable_operation
 } // obelisk::core::coroutine
 
 #endif //AWAITABLE_OPERATION_H
